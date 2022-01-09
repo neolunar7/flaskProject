76dd300e39b694ef32b8f34e5e3bff670883e8fa
piljaechae <piljae@rapportlabs.kr>
2022-01-09 22:36:50 +0900

    Add a function and route

--- app.py
+++ app.py
@@ -8,5 +8,14 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/number')
+def hello_number():
+    return simple_function(3)
+
+
+def simple_function(number):
+    return number * number
+
+
 if __name__ == '__main__':
     app.run()
